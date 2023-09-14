7e2999e8ea79894f58e4dd37449437b827e664a0
amalmohan123 <amalkarikkad@gmail.com>
2023-09-14 16:16:54 +0530

    color

---
+++ lib/helpers/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ColorClass {
+  static const redColor = Colors.red;
+  static const whiteColor = Colors.white;
+  static const blackColor = Colors.black;
+  static const pinkColor = Colors.pink;
+  // static const redColor = Colors.red;
+  // static const redColor = Colors.red;
+  // static const redColor = Colors.red;
+  // static const redColor = Colors.red;
+  // static const redColor = Colors.red;
+  // static const redColor = Colors.red;
+  // static const redColor = Colors.red;
+  // static const redColor = Colors.red;
+  // static const redColor = Colors.red;
+}
